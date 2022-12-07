7d76c66b9aeed3f2947b00703a80b6738b4a4805
garsemar <martigarciaserra@gmail.com>
2022-12-07 20:03:07 +0100

    Day 5

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -1,41 +1,13 @@
 import java.io.File
-
-fun solve(input: String, moveTogether: Boolean) {
-    val (raw_towers, moves) = input.split("\n\n")
-    val towers = raw_towers.split("\n").dropLast(1).asReversed()
-    val stacks: MutableList<MutableList<Char>> = mutableListOf()
-
-    for (j in 1 until towers[0].length step 4) {
-        stacks.add(mutableListOf())
-        for (i in 0 until towers.size)
-            if (towers[i][j] != ' ')
-                stacks.last().add(towers[i][j])
-    }
-
-    val nums = moves
-        .split("\n")
-        .map { it.filter { !it.isLetter() } }
-        .map { it.split("  ") }
-        .map { it.map { it.trim().toInt() } }
-
-    for ((num, from, to) in nums) {
-        if (!moveTogether)
-            for (i in 1..num)
-                stacks[to - 1].add(stacks[from - 1].removeLast())
-        else {
-            val tmp: MutableList<Char> = mutableListOf()
-            for (i in 1..num)
-                tmp.add(stacks[from - 1].removeLast())
-            for (i in 1..num)
-                stacks[to - 1].add(tmp.removeLast())
-
-        }
-    }
-    println(stacks.map { it.last() }.joinToString(""))
-}
+import java.util.Stack
 
 fun main() {
     val file = File("src/main/resources/input5.txt").readText()
-    solve(file, false)
-    solve(file, true)
+    val stack = Stack<Char>()
+
+    createTable("RHMPZ")
+}
+
+fun createTable(stack: String){
+    println(stack.split("").filter { it != "" })
 }
